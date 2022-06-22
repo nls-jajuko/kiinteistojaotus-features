b833decdafad5571752415b05dc1485a4f100a61
Janne Heikkilä <jampukka@users.noreply.github.com>
2022-06-22 21:50:36 +0300

    It's actually find not first
    Also json must be there

--- features.js
+++ features.js
@@ -2,5 +2,5 @@ export async function features(func, endpoint, feat, params) {
     const url = new URL(`${endpoint}/collections/${feat}/items`);
     Object.entries(params).forEach((item) => url.searchParams.set(...item));
 
-    for (let next = url.toString(); next; next = await fetch(next).then(r => r.json()).then(json => func(json) && json?.links?.first(l => l.rel === 'next')?.href ?? 0));
+    for (let next = url.toString(); next; next = await fetch(next).then(r => r.json()).then(json => func(json) && json.links?.find(l => l.rel === 'next')?.href ?? 0));
 }
